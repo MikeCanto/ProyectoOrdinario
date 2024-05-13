4a344bf1470d7f4ada120932091bb639c29309ab
D-Argentum <004091222@anahuac.mx>
2024-05-13 10:49:41 -0600

    Final Changes

--- menu.py
+++ menu.py
@@ -4,14 +4,20 @@ while True:
     print('OPTIONS:\n'
           'A: P1A4 Regresion Lineal\n'
           'B: P1A5 Curvas ROC\n'
-          'C: P1A7 Regresión logística\n'
+          'C: P1A7 Regresión Logística\n'
           'D: P2A3 Cadenas de Markov\n'
-          'E: P2A4 Modelo epidemiológico\n'
-          'F: P2A5 Modelo de inversión\n'
-          'G: P2A7 Neurona artificial\n'
+          'E: P2A4 Modelo Epidemiológico\n'
+          'F: P2A5 Modelo de Inversión\n'
+          'G: P2A7 Neurona Artificial\n'
           'H: P2A8 Red Neuronal\n'
-          'I: P2A9 Red Neuronal tienda de ropa\n'
-          'J: AGS\n'
+          'I: P2A9 Red Neuronal Tienda de Ropa\n'
+          'J: P2A11 AGS Planteamiento del Problema\n'
+          'K: P2A11 AGS Asesor de Inversión\n'
+          'L: Colonia de Hormigas\n'
+          'M: Enjambre de Partículas\n'
+          'N: SIA\n'
+          'X: Exit\n'
+
           )
     choice = input("Enter your choice (or 'X' to exit): ")
 
@@ -45,7 +51,7 @@ while True:
             with Image.open('P1A4_5.png') as img:
                 img.show()
             input("Press Enter to continue...")
-            # ... (Add code for option B)
+
         case "B":
             print("You chose option C: P2A5 Curvas ROC")
             with Image.open('P1A5_1.png') as img:
@@ -112,7 +118,7 @@ while True:
             input("Press Enter to continue...")
             # ... (Add code for option B)
         case "E":
-            print("You chose option E: P2A4 Modelo epidemiológico")
+            print("You chose option E: P2A4 Modelo Epidemiológico")
             with Image.open('P2A4_1.png') as img:
                 img.show()
             input("Press Enter to continue...")
@@ -123,7 +129,7 @@ while True:
                 img.show()
             input("Press Enter to continue...")
         case "F":
-            print("You chose option F: P2A5 Modelo de inversión")
+            print("You chose option F: P2A5 Modelo de Inversión")
             with Image.open('P2A5_1.png') as img:
                 img.show()
             input("Press Enter to continue...")
@@ -135,16 +141,146 @@ while True:
             input("Press Enter to continue...")
         case "G":
             print("You chose option G: P2A7 Neurona artificial")
-            # ... (Add code for option B)
+
+            with Image.open('P2A7_1.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A7_2.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+
+
         case "H":
             print("You chose option H: P2A8 Red Neuronal")
-            # ... (Add code for option B)
+
+            with Image.open('P2A8_1.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A8_2.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A8_3.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A8_4.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A8_5.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A8_6.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+
+
         case "I":
-            print("You chose option I: P2A9 Red Neuronal tienda de ropa")
-            # ... (Add code for option B)
+            print("You chose option I: P2A9 Red Neuronal Tienda de Ropa")
+            with Image.open('P2A9_1.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A9_2.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A9_3.png') as img:
+                img.show()
+            with Image.open('P2A9_4.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A9_5.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+
+
         case "J":
-            print("You chose option J: P2A11 AGS")
-            # ... (Add code for option B)
+            print("You chose option J: P2A11 AGS Planteamiento del Problema")
+            with Image.open('P2A11_PdP_1.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A11_PdP_2.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A11_PdP_3.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A11_PdP_4.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A11_PdP_5.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A11_PdP_6.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+
+
+        case "K":
+            print("You chose option K: P2A11 AGS Asesor de Inversión")
+            with Image.open('P2A11_1.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A11_2.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A11_3.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('P2A11_4.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+
+        case "L":
+            print("You chose option L: Colonia de Hormigas")
+            with Image.open('CdH_1.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('CdH_2.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('CdH_3.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('CdH_4.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+
+        case "M":
+            print("You chose option M: Enjambre de Partículas")
+            with Image.open('EdP_1.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('EdP_2.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('EdP_3.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('EdP_4.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+
+        case "N":
+            print("You chose option N: SIA")
+            with Image.open('SIA_1.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('SIA_2.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('SIA_3.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('SIA_4.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('SIA_5.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+            with Image.open('SIA_6.png') as img:
+                img.show()
+            input("Press Enter to continue...")
+
+
+
         case "X":
             print("Exiting...")
             break  # Exit the loop
